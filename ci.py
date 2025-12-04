4bf19b6ec4335d96800b03af7fcaa1b019e45505
Akshay Kumar <101784969+Kumar0408@users.noreply.github.com>
2025-12-04 09:59:34 +0530

    Update ci.py

--- ci.py
+++ ci.py
@@ -1,4 +1,4 @@
 import wandb
 
 print(f"The wandb version is {wandb.__version__}")
-assert wanddb.__version__=='2.1.01', f"Expected 2.1.01 but got {wanddb.__version__}"
+assert wandb.__version__=='0.23.1', f"Expected 2.1.01 but got {wandb.__version__}"
